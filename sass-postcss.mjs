409f4850981c9fec8dfd5802ced24dc68ba54f5a
MeldSnake <ikeek.33@gmail.com>
2023-01-09 16:22:31 +0100

    Convertion des styles css vers des styles sass.
    Ajout de l'image par defaut des titres.
    
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

---
+++ sass-postcss.mjs
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
+import postcssNested from "postcss-nested";
+import postcssAdvancedVariables from 'postcss-advanced-variables';
+import sass from "sass";
+import fs from "fs";
+import fsp from "fs/promises";
+import path from "path";
+import { exit } from "process";
+
+const watchSignal = new AbortController();
+const postCssProcessor = postcss([autoprefixer, postcssNested, postcssAdvancedVariables]);
+
+/**
+ * @param {string} filePath
+ * @param {string} destPath
+ */
+async function processSCSS(filePath, destPath) {
+    const compiled = await sass.compileAsync(
+        filePath, {
+            sourceMap: false,
+        }
+    );
+    const processed = await postCssProcessor
+        .process(compiled.css, {
+            map: false,
+            from: undefined,
+        });
+    let fd;
+    try {
+        fd = await fsp.open(destPath, "w");
+        await fd.write(processed.css, null, "utf8");
+    } finally {
+        await fd.close();
+    }
+}
+
+/**
+ * @param {string} srcPath
+ * @param {string} destPath
+ * @param {string} filePath
+ */
+async function processFile(srcPath, destPath, filePath) {
+    console.log("Processing %s -> %s", filePath, destPath);
+    await fsp.mkdir(destPath, {
+        recursive: true,
+    });
+    const fullFilePath = path.join(srcPath, filePath);
+    const stat = await fsp.stat(fullFilePath);
+    
+    if (stat.isFile() && fullFilePath.endsWith('.scss')) {
+        const destFile = path.join(destPath, filePath.slice(0, filePath.lastIndexOf('.scss')) + ".css");
+        return await processSCSS(fullFilePath, destFile);
+    }
+}
+
+async function main() {
+    await fsp.readdir("./sass")
+        .then(async (files) => {
+            await Promise.allSettled(files.map(processFile.bind(null, "./sass", "./public/styles")));
+        });
+
+    if (["-w", "--watch"].includes(process.argv[2])) {
+        try {
+            const watcher = fsp.watch("./sass", {
+                signal: watchSignal.signal,
+            });
+            for await (const event of watcher) {
+                await processFile("./sass", "./public/styles", event.filename);
+            }
+        } catch (err) {
+            if (err.name === "AbortError") {
+                return Promise.resolve();
+            }
+            throw err;
+        }
+    }
+}
+
+await main();
